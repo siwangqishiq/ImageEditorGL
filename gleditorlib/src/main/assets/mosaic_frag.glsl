996b77c851cb7266a621af5483d5e009f753a3e2
yixin.panyi <panyi@yixin.im>
2022-09-02 11:09:03 +0800

    马赛克算法 shader实现

--- gleditorlib/src/main/assets/mosaic_frag.glsl
+++ gleditorlib/src/main/assets/mosaic_frag.glsl
@@ -11,27 +11,33 @@ in vec3 originCoord;
 
 layout(location = 0) out vec4 outColor;
 
-vec2 mosaicEffect(){
+vec3 mosaicEffect(){
     int leftIndex = int(originCoord.x) / int(mosaicSize);
     int rightIndex = leftIndex + 1;
     int bottomIndex = int(originCoord.y) / int(mosaicSize);
     int topIndex = bottomIndex + 1;
 
-    vec2 leftBottomPoint;
-    leftBottomPoint.x =  float(leftIndex) * mosaicSize;
-    leftBottomPoint.y = float(bottomIndex) * mosaicSize;
+    vec3 colorSum;
+    int count=0;
+    for(int i = leftIndex ; i <= rightIndex;i++){
+        for(int j = bottomIndex; j<= topIndex ;j++){
+            float x = float(i) * mosaicSize;
+            float y = float(j) * mosaicSize;
+            vec2 uv = vec2(x / originImageWidth ,1.0f - y / originImageHeight);
+            vec4 originColor = texture(baseImageTexture , uv);
 
-    vec2 rightTopPoint;
-    rightTopPoint.x = float(rightIndex) * mosaicSize;
-    rightTopPoint.y = float(topIndex) * mosaicSize;
-
-    return vec2((leftBottomPoint.x + rightTopPoint.x) / 2.0f , (leftBottomPoint.y + rightTopPoint.y) / 2.0f);
+            colorSum = colorSum + originColor.rgb;
+            count++;
+        }//end for j
+    }//end for i
+    return vec3(colorSum.r / float(count) , colorSum.g / float(count) ,  colorSum.b / float(count));
 }
 
 void main(){
-    vec2 mosaicPoint = mosaicEffect();
-    vec2 uv = vec2(mosaicPoint.x / originImageWidth ,1.0f - mosaicPoint.y / originImageHeight);
-    vec4 originColor = texture(baseImageTexture , uv);
-    outColor = originColor;
+//    vec2 mosaicPoint = mosaicEffect();
+//    vec2 uv = vec2(mosaicPoint.x / originImageWidth ,1.0f - mosaicPoint.y / originImageHeight);
+//    vec4 originColor = texture(baseImageTexture , uv);
+//    outColor = originColor;
 //    outColor = vec4(0.0f , 0.0f ,0.0f , 1.0f);
+    outColor = vec4(mosaicEffect() , 1.0f);
 }
